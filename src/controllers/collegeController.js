ffdae510f4a897c161056ad002db9d331134efe9
lalan992 <lalankumar992588@gmail.com>
2022-09-15 21:22:21 +0530

    project2day3update6

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -3,47 +3,56 @@ const internModel = require("../models/internModel");
 const validator = require("../utils/validator");
 
 const registerCollege = async function (req, res) {
-  const requestBody = req.body;
-  const { name, fullName, logoLink } = requestBody;
-  if (!validator.isValidRequestBody(requestBody)) {
-    return res
-      .status(400)
-      .send({ status: false, message: "Please provide college details ..." });
-  }
-  if (requestBody.isDeleted == true) {
-    return res.status(400).send({
-      status: false,
-      message: "isDeleted must be false...",
+  try {
+    const requestBody = req.body;
+    const { name, fullName, logoLink } = requestBody;
+    if (!validator.isValidRequestBody(requestBody)) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Please provide college details ..." });
+    }
+    if (requestBody.isDeleted == true) {
+      return res.status(400).send({
+        status: false,
+        message: "isDeleted must be false...",
+      });
+    }
+    if (!validator.isValid(name)) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Please provide college name ..." });
+    }
+    if (!validator.isValidLink(logoLink)) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Please provide valid logolink ..." });
+    }
+    if (!validator.isValid(fullName)) {
+      return res
+        .status(400)
+        .send({
+          status: false,
+          message: "Please provide college fullname ...",
+        });
+    }
+    const college = await collegeModel.findOne({ name: name });
+    if (college) {
+      return res
+        .status(400)
+        .send({ status: false, message: "college name already exists..." });
+    }
+    const collegeData = await collegeModel.create(requestBody);
+    ["_id", "updatedAt", "createdAt", "__v"].forEach(
+      (x) => delete collegeData._doc[x]
+    );
+    res.status(201).send({
+      status: true,
+      data: collegeData,
+      message: "College created successfully..",
     });
+  } catch (error) {
+    return res.status(500).send({ status: false, msg: error.message });
   }
-  if (!validator.isValid(name)) {
-    return res
-      .status(400)
-      .send({ status: false, message: "Please provide college name ..." });
-  }
-  if (!validator.isValidLink(logoLink)) {
-    return res
-      .status(400)
-      .send({ status: false, message: "Please provide valid logolink ..." });
-  }
-  if (!validator.isValid(fullName)) {
-    return res
-      .status(400)
-      .send({ status: false, message: "Please provide college fullname ..." });
-  }
-  const college = await collegeModel.findOne({ name: name });
-  if (college) {
-    return res
-      .status(400)
-      .send({ status: false, message: "college name already exists..." });
-  }
-  const collegeData = await collegeModel.create(requestBody);
-  ["_id","updatedAt","createdAt","__v"].forEach((x)=>delete collegeData._doc[x]);
-  res.status(201).send({
-    status: true,
-    data: collegeData,
-    message: "College created successfully..",
-  });
 };
 
 const collegeDetails = async function (req, res) {
